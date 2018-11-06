5d2bd9ffdd4dbf5f1932afa21c60102a969dfe2b
Darren Arcangel <darrenarcangel@gmail.com>
2018-11-06 11:51:02 -0800

    tweak google strategy settings

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
 //keys.js - figure out what set of credentilas to return
 if (process.env.NODE_ENV === 'production') {
-    //we are in production - return the prod set of keys
+    //we are in production - return the prod keys
     module.exports = require('./prod');
 } else {
     // we are in development - return the dev keys
